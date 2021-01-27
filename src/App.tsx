78052704c1c73e35bb5679277d4ed788a820bd61
berni97 <bernhardelias.rabiser@stud.th-rosenheim.de>
2021-01-27 17:24:47 +0100

    Fixed issue when including semantic-ui-react CSS files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import './App.global.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header, Segment, Input, Dropdown } from 'semantic-ui-react';
 
