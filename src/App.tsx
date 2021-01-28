7c83b0495a2c8a699ae513b5945a37aabe04f101
berni97 <bernhardelias.rabiser@stud.th-rosenheim.de>
2021-01-28 14:56:56 +0100

    Built basic version of the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Header, Segment, Input, Dropdown } from 'semantic-ui-react';
+import { Header, Grid, Button } from 'semantic-ui-react';
+import SportSelection from './SportSelection';
+import KilometerFormular from './KilometerFormular';
 
-const KilometerFormular = () => {
-  const options = [
-    { key: 'run', text: 'Laufen', value: 'run' },
-    { key: 'bike', text: 'Radfahren', value: 'bike' },
-    { key: 'climb', text: 'Bergsteigen', value: 'climb' },
-  ];
-  return (
-    <Input
-      action={
-        <Dropdown button basic floating options={options} defaultValue="run" />
-      }
-      icon="stopwatch"
-      iconPosition="left"
-      placeholder="Anzahl km"
-    />
-  );
-};
 const Home = () => {
+  const [, setSelectedSport] = React.useState('');
+
+  const handleEnterKM = (km: number) => {
+    // eslint-disable-next-line no-console
+    console.log('You entered: ', km);
+  };
   return (
     <div>
-      <Header as="h1">Willkommen!</Header>
-      <Segment>
-        <KilometerFormular />
-      </Segment>
+      <Grid>
+        <Grid.Row />
+        <Grid.Row>
+          <Grid.Column width={3} />
+          <Grid.Column width={10} textAlign="center">
+            <Header as="h1">{'<FitnessApp/>'}</Header>
+          </Grid.Column>
+          <Grid.Column width={3} />
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={3} />
+
+          <Grid.Column width={10} textAlign="center">
+            <SportSelection
+              onSelected={(chosenSport: string) => {
+                setSelectedSport(chosenSport);
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column width={3} />
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column width={5} />
+
+          <Grid.Column textAlign="left" width={3}>
+            <KilometerFormular onEnter={handleEnterKM} />
+          </Grid.Column>
+          <Grid.Column textAlign="right" width={3}>
+            <Button>Jetzt speichern!</Button>
+          </Grid.Column>
+
+          <Grid.Column width={5} />
+        </Grid.Row>
+      </Grid>
     </div>
   );
 };
